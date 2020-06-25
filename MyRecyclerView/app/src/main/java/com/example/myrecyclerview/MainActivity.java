8e1eaa11140825b3e7fc66646bfc8f6cbdf92202
Riko <mr.rikowijayanto@gmail.com>
2020-06-26 05:51:36 +0700

    Rotation Layout Design and Code Improvement

--- MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.java
+++ MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -16,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView rvHeroes;
 
+    private final String STATE_TITLE = "state_string";
+    private final String STATE_LIST = "state_list";
+    private final String STATE_MODE = "state_mode";
+    private int mode;
+
     private String title = "Mode List";
 
     private ArrayList<Hero> list = new ArrayList<>();
@@ -28,10 +33,25 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes = findViewById(R.id.rv_heroes);
         rvHeroes.setHasFixedSize(true);
 
-        list.addAll(getListHeroes());
-        showRecyclerList();
+        if (savedInstanceState == null) {
 
-        setActionBarTitle(title);
+            setActionBarTitle(title);
+            list.addAll(getListHeroes());
+            showRecyclerList();
+            mode = R.id.action_list;
+
+        } else {
+
+            title = savedInstanceState.getString(STATE_TITLE);
+            ArrayList<Hero> stateList = savedInstanceState.getParcelableArrayList(STATE_LIST);
+            int stateMode = savedInstanceState.getInt(STATE_MODE);
+            setActionBarTitle(title);
+            if (stateList != null) {
+                list.addAll(stateList);
+            }
+
+            setMode(stateMode);
+        }
 
 
     }
@@ -83,11 +103,23 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         setMode(item.getItemId());
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_TITLE, title);
+        outState.putParcelableArrayList(STATE_LIST, list);
+        outState.putInt(STATE_MODE, mode);
+    }
+
+
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
